6819597f0c23c82d275a2a04e53a64efd862d228
fusillicode <fusillicode@gmail.com>
2017-05-27 11:38:31 +0200

    config guardian and ueberauth

--- config/config.exs
+++ config/config.exs
@@ -22,13 +22,26 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Uberauth config
+# Ueberauth config
 config :ueberauth, Ueberauth,
+  base_path: "/",
   providers: [
     identity: {Ueberauth.Strategy.Identity, [
-      callback_methods: ["POST"]
+      request_path: "",
+      callback_path: "",
+      callback_methods: ["POST"],
+      uid_field: :email,
+      nickname_field: :email
     ]}
   ]
+
+# Guardian config
+config :guardian, Guardian,
+  issuer: "Metro",
+  ttl: { 30, :days },
+  allowed_drift: 2000,
+  secret_key: "J1debCKFp5ahjLSQIKouXC5Tr4lBy+Z7xDk90x4QO1L2pxsoAmDO48X659Td0Bfa",
+  serializer: Metro.GuardianSerializer
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
