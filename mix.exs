318eef3e5392a946248e6f4495e9f86321a7ad04
fusillicode <fusillicode@gmail.com>
2017-05-27 11:37:32 +0200

    add guardian + comeonin

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Metro.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
+    [{:phoenix, "~> 1.3.0-rc", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:postgrex, ">= 0.0.0"},
@@ -37,6 +37,8 @@ defmodule Metro.Mixfile do
      {:gettext, "~> 0.11"},
      {:ueberauth, "~> 0.4"},
      {:ueberauth_identity, "~> 0.2"},
+     {:guardian, "~> 0.14"},
+     {:comeonin, "~> 3.0"},
      {:cowboy, "~> 1.0"}]
   end
 
