fa63f042e78f730e98890885f2da288e0e9cc400
fusillicode <fusillicode@gmail.com>
2017-07-24 09:07:02 +0200

    add per-page-js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,21 @@ import "phoenix_html"
 
 // import socket from "./socket"
 require('bootstrap-loader');
+
+import loadView from './views/loadView';
+
+function handleDOMContentLoaded() {
+  const viewName = document.getElementsByTagName('body')[0].dataset.jsViewPath;
+
+  const view = loadView(viewName);
+  view.mount();
+
+  window.currentView = view;
+}
+
+function handleDocumentUnload() {
+  window.currentView.unmount();
+}
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded, false);
+window.addEventListener('unload', handleDocumentUnload, false);
